2d1b688c18c50a4cfa6511fa77c3df6a9f6a23be
Ross Gray <rossgray@TechEd-0008.techeducators.co.uk>
2024-01-16 10:49:25 +0000

    second

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-let array = [1, 2, 3, 4, 5, 6, 7];
-let sum = 0;
+// let array = [1, 2, 3, 4, 5, 6, 7];
+// let sum = 0;
 
-for (let i = 0; i < array.length; i++) {
-  sum += array[i];
-}
+// for (let i = 0; i < array.length; i++) {
+//   sum += array[i];
+// }
 
-console.log(sum);
+// console.log(sum);
+
+const person = {
+  name: "Bob",
+  age: 30,
+  favouriteColour: "Red",
+};
+
+person.favouriteFood = "Pizza";
+
+console.log(person.favouriteFood);
+
+console.table(person);
+
+const car = {
+  make: "Toyota",
+  model: "Yaris",
+  modelVariant: "Gr Yaris",
+  colour: "Pearl White",
+};
+
+console.table(car);
+
+const book = {
+  title: "Much ado About Nothing",
+  author: "The Bard",
+  numberOfPages: 550,
+};
+
+console.table(book);
